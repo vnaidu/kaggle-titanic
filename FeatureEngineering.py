566348d8375fa390508907448429e746eed9b960
Victor Naidu <victor@vnaidu.com>
2017-05-14 16:04:45 -0700

    Add New Feature (no_family)
    Feature reflects passengers that have no family members on-board:
    `family_size` equals 1 -> T/F -> 1/0 -> `no_family`

--- FeatureEngineering.py
+++ FeatureEngineering.py
@@ -10,3 +10,8 @@ df = pd.concat([df_train, df_test], axis=0)
 # `family_size` is calculated using the following features: `sibsp` & `parch`
 df.family_size = df.sibsp + df.parch + 1
 
+# Add New Feature (no_family)
+# Feature reflects passengers that have no family members on-board:
+# `family_size` equals 1 -> T/F -> 1/0 -> `no_family`
+
+df.no_family = np.array(df.family_size == 1).astype(int)
