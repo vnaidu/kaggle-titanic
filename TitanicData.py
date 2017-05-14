d5007da66f8fb179d3cefd1668d767d4e9a3d9d5
Victor Naidu <victor@vnaidu.com>
2017-05-13 21:16:18 -0700

    Add Combined DataFrame by Merging Train/Test Sets
    Before merging, a new column was assigned to both sets with discrete values ['Train'/'Test'] that correspond to the set an observation is inclusive of.

--- TitanicData.py
+++ TitanicData.py
@@ -11,3 +11,10 @@ filepath_test = 'Data/test.csv'
 # Load train/test datasets as Pandas DataFrames
 df_train = pd.read_csv('Data/train.csv', index_col='PassengerId')
 df_test = pd.read_csv('Data/test.csv', index_col='PassengerId')
+
+# Assign dataset labels as a new column
+df_train['Dataset'] = 'Train'
+df_test['Dataset'] = 'Test'
+
+# Create a combined DataFrame by merging train/test sets
+df_combined = pd.concat([df_train, df_test], axis=0)
