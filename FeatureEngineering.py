ce7a64b17372e56d7da25f53d1df1ae4099bc137
Victor Naidu <victor@vnaidu.com>
2017-05-14 15:57:30 -0700

    Add New Feature (family_size)
    `family_size` is calculated using the following features: `sibsp` & `parch`

--- FeatureEngineering.py
+++ FeatureEngineering.py
@@ -6,3 +6,7 @@ df_train = pd.read_csv('Data/train.csv', index_col='PassengerId')
 df_test = pd.read_csv('Data/test.csv', index_col='PassengerId')
 df = pd.concat([df_train, df_test], axis=0)
 
+# Add New Feature (family_size)
+# `family_size` is calculated using the following features: `sibsp` & `parch`
+df.family_size = df.sibsp + df.parch + 1
+
