df4ff38135c80d988b513535d4f192e09e8cdc37
Victor Naidu <victor@vnaidu.com>
2017-05-16 04:34:54 -0700

    Make Functional and Define a Class TitanicFunctions

--- FeatureEngineering.py
+++ FeatureEngineering.py
@@ -1,51 +1,119 @@
-# coding=utf-8
+import pandas as pd
+import numpy as np
 
-# Load train/test datasets as Pandas DataFrames
-df_train = pd.read_csv('Data/train.csv', index_col='PassengerId')
-df_test = pd.read_csv('Data/test.csv', index_col='PassengerId')
-df = pd.concat([df_train, df_test], axis=0)
 
-# Add New Feature (family_size)
-# `family_size` is calculated using the following features: `sibsp` & `parch`
-df.family_size = df.sibsp + df.parch + 1
+class TitanicFeatures(object):
+    """Summary of class here.
 
-# Add New Feature (no_family)
-# Feature reflects passengers that have no family members on-board:
-# `family_size` equals 1 -> T/F -> 1/0 ==> `no_family`
-df.no_family = np.array(df.family_size == 1).astype(int)
+    Longer class information....
 
-# Add New Feature (name_length)
-# `name_length` equals string length of `name` feature.
-df.name_length = df.name.str.len()
+    Attributes:
+        name: A 
+        dataset: A 
+    """
 
-# Add New Feature (name_length_group)
-# `name_length -> 4x bins ==> `name_length_group`
-df.name_length_group = pd.cut(df.name_length, 4, labels=False)
+    def __init__(self, df):
+        self.get_all = self.all_features(df)
 
-# Add New Feature (deck)
-# `deck` is calculated by extracting the first character of 'cabin' feature.
-# If 'cabin' is null, than `deck` value for that observation is set to '?'.
-df.deck = df.cabin.fillna('?').str.slice(0, 1)
+    @staticmethod
+    def family_size(sibsp, parch):
+        """
+        Add New Feature (family_size):
+        `family_size` is calculated using the following features: `sibsp` & `parch`
+        :param sibsp: 
+        :param parch: 
+        :return: family_size
+        """
+        family_size = sibsp + parch + 1
+        return family_size
 
-'''
-The resulting feature `title` is obtained by performing the 
-following operations on `name`:
- (1) splitting ^*(string) on ',' & extracting 1st item
- (2) splitting ^(1) on '.' & extracting 1st item
- (3) stripping ^(2) of any leading/trailing whitespace characters
- (1) mapping ^(3) to title groups using `title_map`(dict)
-'''
-def kv_to_vk(d):
-    return {t: k for k, v in d.items() for t in v}
+    @staticmethod
+    def no_family(family_size):
+        """
+        Add New Feature (family_size):
+        Feature reflects passengers that have no family members on-board.
+        :param family_size: 
+        :return: 
+        """
+        # `no_family` <= [1|0] <- [T|F] <- (family_size == 1)
+        no_family = np.array(family_size == 1).astype(int)
+        return no_family
 
-title_map = kv_to_vk({
-    'Officer': ['Capt', 'Col', 'Major', 'Rev'],
-    'Royalty': ['Sir', 'Lady', 'the Countess',
-                'Dona', 'Jonkheer', 'Don'],
-    'Dr': ['Dr'], 'Master': ['Master'], 'Mr': ['Mr'],
-    'Miss': ['Ms', 'Miss', 'Mlle'], 'Mrs': ['Mrs', 'Mme']})
-df.title = (df.name
-            .str.split(',').str.get(1)
-            .str.split('.').str.get(0)
-            .str.strip()
-            .map(title_map))
+    @staticmethod
+    def name_length(name):
+        """
+        Add New Feature (name_length):
+        `name_length` equals string length of `name` feature.
+        :param name: 
+        :return: name_length
+        """
+        name_length = name.str.len()
+        return name_length
+
+    @staticmethod
+    def name_length_group(name_length):
+        """
+        Add New Feature (name_length_group):
+        `name_length -> 4x bins ==> `name_length_group`
+        :param name_length: 
+        :return: name_length_group
+        """
+        name_length_group = pd.cut(name_length, 4, labels=False)
+        return name_length_group
+
+    @staticmethod
+    def deck(cabin):
+        """
+        Add New Feature (deck):
+    
+        :param cabin: 
+        :return: deck
+        """
+        deck = cabin.fillna('?').str.slice(0, 1)
+        return deck
+
+    @staticmethod
+    def title(name):
+        """
+        Add New Feature (title):
+        The resulting feature `title` is obtained by performing the 
+        following operations on `name`:
+             (1) splitting ^*(string) on ',' & extracting 1st item
+             (2) splitting ^(1) on '.' & extracting 1st item
+             (3) stripping ^(2) of any leading/trailing whitespace characters
+             (1) mapping ^(3) to title groups using `title_map`(dict)
+        :param name: 
+        :return: title
+        """
+
+        def kv_to_vk(d):
+            return {t: k for k, v in d.items() for t in v}
+
+        title_map = kv_to_vk({
+            'Officer': ['Capt', 'Col', 'Major', 'Rev'],
+            'Royalty': ['Sir', 'Lady', 'the Countess',
+                        'Dona', 'Jonkheer', 'Don'],
+            'Dr': ['Dr'], 'Master': ['Master'], 'Mr': ['Mr'],
+            'Miss': ['Ms', 'Miss', 'Mlle'], 'Mrs': ['Mrs', 'Mme']})
+
+        title = (name
+                 .str.split(',').str.get(1)
+                 .str.split('.').str.get(0)
+                 .str.strip()
+                 .map(title_map))
+        return title
+
+    @classmethod
+    def all_features(cls, df):
+        """
+        Add all available new features.
+        :param data: 
+        :return: 
+        """
+        df['family_size'] = cls.family_size(sibsp=df.sib_sp, parch=df.parch)
+        df['no_family'] = cls.no_family(family_size=df.family_size)
+        df['name_length'] = cls.name_length(name=df.name)
+        df['name_length_group'] = cls.name_length_group(name_length=df.name_length)
+        df['deck'] = cls.deck(cabin=df.cabin)
+        df['title'] = cls.title(name=df.name)
+        return df
