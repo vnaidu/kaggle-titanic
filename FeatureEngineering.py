7517c7a81354a984fe00518e100a41022e4b08dc
Victor Naidu <victor@vnaidu.com>
2017-05-16 01:33:33 -0700

    Add New Features: name_length, name_length_group
    `name_length` equals string length of `name` feature.
    `name_length -> 4x bins ==> `name_length_group`

--- FeatureEngineering.py
+++ FeatureEngineering.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 
-#
 # Load train/test datasets as Pandas DataFrames
 df_train = pd.read_csv('Data/train.csv', index_col='PassengerId')
 df_test = pd.read_csv('Data/test.csv', index_col='PassengerId')
@@ -12,6 +11,15 @@ df.family_size = df.sibsp + df.parch + 1
 
 # Add New Feature (no_family)
 # Feature reflects passengers that have no family members on-board:
-# `family_size` equals 1 -> T/F -> 1/0 -> `no_family`
-
+# `family_size` equals 1 -> T/F -> 1/0 ==> `no_family`
 df.no_family = np.array(df.family_size == 1).astype(int)
+
+# Add New Feature (name_length)
+# `name_length` equals string length of `name` feature.
+df.name_length = df.name.str.len()
+
+# Add New Feature (name_length_group)
+# `name_length -> 4x bins ==> `name_length_group`
+df.name_length_group = pd.cut(df.name_length, 4, labels=False)
+
+
